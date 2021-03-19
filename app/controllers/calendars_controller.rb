8056687e13262628e83f04195b51793b2eb91f37
takuo-maeda <cafeacb@olivesheep34.sakura.ne.jp>
2021-03-19 16:02:16 +0900

    issue３

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -28,15 +28,16 @@ class CalendarsController < ApplicationController
     @week_days = []
 
     plans = Plan.where(date: @todays_date..@todays_date + 6)
+    # 与えられた条件にマッチするレコードをすべて返す。
+    # 範囲オブジェクトを作成するには「..」演算子を使用します。
 
     7.times do |x|
       today_plans = []
       plans.each do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
-      days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans}
+      days = { month: (@todays_date + x).month, date: (@todays_date + x).day, plans: today_plans}
       @week_days.push(days)
     end
-
   end
 end
